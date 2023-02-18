2cb053a3ad7eab4b8530fd6d73ee89308cdcb1f0
lagrandecode <seunogunmolufirst1@gmail.com>
2023-02-19 00:05:21 +0100

    "commit"

--- payslip/models.py
+++ payslip/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Grade(models.Model):
+    positon = models.CharField(max_length=250)
+    basic_salary = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.positon
+
+
+class staff(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField()
+    hour = models.PositiveIntegerField(default=0)
+    gross_salary = models.PositiveIntegerField(default=0)
+    grade_status = models.ForeignKey(Grade, on_delete=models.CASCADE)
+
+    def save(self, *args,**kwargs):
+        self.gross_salary = (self.hour * self.grade_status.basic_salary)
+        return super().save(*args,**kwargs)
+    
+
+    def __str__(self):
+        return self.first_name
