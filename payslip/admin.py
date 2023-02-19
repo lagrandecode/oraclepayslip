b17e5b0a5b81dcc7f17c1ea432e9926cd9745234
lagrandecode <seunogunmolufirst1@gmail.com>
2023-02-19 22:18:21 +0100

    "commit"

--- payslip/admin.py
+++ payslip/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import Grade, staff
 
 # Register your models here.
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','gross_salary')
 
-admin.site.register(Grade)
-admin.site.register(staff)
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('positon','basic_salary')
+
+admin.site.register(Grade,GradeAdmin)
+admin.site.register(staff,StaffAdmin)
 
 
 
