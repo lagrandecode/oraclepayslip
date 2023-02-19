137d3d0af03a9dc874dae7699f5d8e27f510888b
lagrandecode <seunogunmolufirst1@gmail.com>
2023-02-19 22:44:36 +0100

    "commit"

--- payslip/views.py
+++ payslip/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from .models import staff
 
 # Create your views here.
 
 def home(request):
+    getstaff = staff.objects.all()
+
     context = {
-
-
+        'getstaff':getstaff
     }
     return render(request,'home.html',context)
 
