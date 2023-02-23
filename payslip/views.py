085c3d089102f452d619988e6435a87de6fb7503
lagrandecode <seunogunmolufirst1@gmail.com>
2023-02-23 11:55:03 +0100

    "commit"

--- payslip/views.py
+++ payslip/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from .models import staff
 
+#for geting the pdf 
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
+
 # Create your views here.
 
 def home(request):
@@ -14,8 +22,22 @@ def home(request):
 
 def payslip(request,pk):
     pay = staff.objects.get(id=pk)
+    template_path = 'payslip.html'
     context = {
         'pay':pay
     }
-    return render(request,'payslip.html',context)
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="report.pdf"'
+    template = get_template(template_path)
+    html = template.render(context)
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funny view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
+
+
 
