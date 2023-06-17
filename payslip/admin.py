276a459bbc01c1bc2d4ae1e465bef94916933f4d
ogunmolu oluwaseun <58033364+lagrandecode@users.noreply.github.com>
2023-06-17 21:34:55 +0100

    Update admin.py

--- payslip/admin.py
+++ payslip/admin.py
@@ -6,7 +6,7 @@ class StaffAdmin(admin.ModelAdmin):
     list_display = ('first_name','last_name','gross_salary')
 
 class GradeAdmin(admin.ModelAdmin):
-    list_display = ('positon','basic_salary')
+    list_display = ['positon','basic_salary']
 
 admin.site.register(Grade,GradeAdmin)
 admin.site.register(staff,StaffAdmin)
